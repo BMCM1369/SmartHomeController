c95661dcb114692f2b568552ac2eec10d45cbd4c
Ben McManus <McManus-B8@ulster.ac.uk>
2025-11-10 16:09:27 +0000

    Created Login Method

--- SmartHomeController/User.cs
+++ SmartHomeController/User.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace SmartHomeController
 {
-    internal class User
+    public class User
     {
+        private int userId;
+        private string userName;
+        private string password;
+        private string contactInfo;
+        private bool isLoggedIn;
+        public int UserId { 
+            get { return userId; } 
+            set { userId = value; } }
+
+        public string UserName { 
+            get { return userName; } 
+            set { userName = value; } }
+
+        public string ContactInfo { 
+            get { return contactInfo; } 
+            set {  contactInfo = value; } }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; } }
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set { isLoggedIn = value; }
+        }
+
+        public bool Login(string userName, string password)
+        {
+            if (UserName == userName && Password == password) {
+                IsLoggedIn = true;
+                Console.WriteLine("Login Successful, Welcome" + userName);
+
+            }
+            else
+            {
+                IsLoggedIn = false;
+                Console.WriteLine("Login failed, try again");
+            }
+            return IsLoggedIn;
+        }
     }
+
 }
